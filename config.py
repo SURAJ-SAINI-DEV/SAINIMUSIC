a6ba322560400f99fc67c6cfb0f2db6936c5d193
SURAJ SAINI <161472575+SURAJ-SAINI-DEV@users.noreply.github.com>
2024-05-22 11:58:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_USERNAME = getenv("BOT_USERNAME" , "@SAINI_MUSICBOT")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 699999999999990))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
