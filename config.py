56c6f95498b0e1314cfd6aae401d9f93ad71a7f0
SURAJ SAINI <161472575+SURAJ-SAINI-DEV@users.noreply.github.com>
2024-07-13 19:12:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/SURAJ-SAINI-DEV/SAINIMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_MAIN", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
